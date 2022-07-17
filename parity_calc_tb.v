399c377c6baab6f39bcae653857be73fefd3b91c
Puneeth <kpuneeth714@gmail.com>
2022-07-18 01:09:13 +0530

    minor changes

--- parity_calc_tb.v
+++ parity_calc_tb.v
@@ -1,4 +1,4 @@
-//`include "parity_calc.v"
+`include "parity_calc.v"
 module parity_calc_tb;
 parameter WIDTH=10;
 
@@ -8,7 +8,7 @@ wire out;
 initial begin
     clk=0;
 end
-parity_calc p1(stream,clk,out);
+parity_calc  #(.WIDTH(WIDTH)) p1(stream,clk,out);
 // create stream using random number generator
 
 always #10 clk=~clk;
@@ -17,7 +17,7 @@ initial begin
     // $dumpvars(0,parity_calc_tb,stream,clk,out);
     stream=101001001;
     $monitor("stream %b out %b",stream,out);
-    repeat(200) #10 stream=stream+1;
+    repeat(200) #10 stream=stream+1'b1;
     $finish;
 end
 
